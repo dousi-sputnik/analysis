c8c1f68bf45fa79db3609487f1e35c80933dca72
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2023-09-17 10:16:23 +0900

    fix code create_bulk action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,34 +49,38 @@ class ItemsController < ApplicationController
       handle_errors(@item, @analysis_session.errors.full_messages.join(", "))
       return
     end
-
+  
     if params[:bulk_input].blank?
       @item ||= current_user.items.build(analysis_session: @analysis_session)
       handle_errors(@item, "Excelデータペーストエリアは空ではありませんか？")
       return
     end
-
-    error_messages_for_bulk_data = validate_bulk_data(params[:bulk_input])
+  
+    item_validator = Item.new(bulk_data: params[:bulk_input])
+    item_validator.validate_bulk_data
+    error_messages_for_bulk_data = item_validator.errors.full_messages
     unless error_messages_for_bulk_data.empty?
       @item ||= current_user.items.build(analysis_session: @analysis_session)
       handle_errors(@item, error_messages_for_bulk_data.join(", "))
       return
     end
-
+  
     unless @analysis_session.save
+      @item ||= current_user.items.build(analysis_session: @analysis_session)
       handle_errors(@item, @analysis_session.errors.full_messages.join(", "))
       return
     end
-
+  
     error_messages = Item.create_from_bulk(params[:bulk_input], current_user, @analysis_session)
   
     if error_messages.empty?
       @analysis_session.analysis!
       redirect_to analysis_session_path(@analysis_session), notice: "分析が完了しました。"
     else
+      @item ||= current_user.items.build(analysis_session: @analysis_session)
       handle_errors(@item, "次の商品の登録に失敗しました: #{error_messages.join(', ')}")
     end
-  end 
+  end  
 
   private
 
@@ -92,33 +96,14 @@ class ItemsController < ApplicationController
     if item.nil?
       respond_to do |format|
         format.html { render :new, alert: message }
-        format.js { render js: "alert('#{j message}');" }
+        format.js { render 'items/error', locals: { error_message: message }, status: :unprocessable_entity }
       end
     else
       item.errors.add(:base, message)
-      puts item.errors.full_messages
       respond_to do |format|
         format.html { render :new }
         format.js { render 'items/error', locals: { item: item }, status: :unprocessable_entity }
       end
     end
-  end
-
-  def validate_bulk_data(bulk_data)
-    rows = bulk_data.split("\n")
-    error_messages = []
-  
-    if rows.size < 1 || rows.size > 100
-      error_messages << "Excelデータは最低1行、最大100行までです。"
-    end
-  
-    rows.each_with_index do |row, index|
-      data = row.split("\t")
-      unless data.length == 3
-        error_messages << "Excelデータの行 #{index + 1} はデータが不正です。"
-      end
-    end
-  
-    error_messages
-  end
+  end  
 end
