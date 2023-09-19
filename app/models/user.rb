2a1f230cc578d2713faec8eaeab962f26aa9545c
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2023-09-19 20:58:35 +0900

    create adminsystem complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules without :validatable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
+  # Custom password validation
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+  
+  validates :password, presence: true, if: :password_required?,
+            length: { in: 6..12 },
+            format: { with: VALID_PASSWORD_REGEX,
+                      message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります" }
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  
   has_many :analysis_sessions
   has_many :items
   has_many :analysis_results, through: :analysis_sessions
+
+  private
+
+  def password_required?
+    password.present? || password_confirmation.present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
@@ -12,4 +11,5 @@ Rails.application.routes.draw do
     end
   end
   resources :analysis_sessions, only: [:show, :destroy]
+  get '/users', to: redirect('/users/sign_up')
 end
