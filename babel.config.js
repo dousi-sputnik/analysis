8b7bfe2ccc58e1cf29804ef69b7ad69ca8c76598
中田　拓弥 <sputnik_u@nakatatakuwatarus-MacBook-Pro.local>
2023-10-10 14:12:47 +0900

    change babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -62,7 +62,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-private-property-in-object',
         {
-          loose: true
+          loose: false
         }
       ],
       [
